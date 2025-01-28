a52df740a0cb43290663d276c51719de736c6408
Arvin-Ullvan-NTI-2C <151524993+Arvin-Ullvan-NTI-2C@users.noreply.github.com>
2025-01-28 16:03:28 +0100

    starting

--- app.rb
+++ app.rb
@@ -2,4 +2,58 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
+
+
+enable :sessions
+
+get('/showlogin') do
+  slim(:login)
+end
+
+get('/') do
+  slim(:register)
+end
+
+post('/login') do
+  username = params[:username]
+  password = params[:password] 
+  db = SQLite3::Database.new('db/db.db')
+  db.results_as_hash = true 
+  result = db.execute("SELECT * FROM users WHERE username = ?", [username]).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+
+  if BCrypt::Password.new(pwdigest) == password 
+    session[:id] = id
+    redirect('/index')
+  else
+    "FEL LÖSEN!"
+  end
+
+end
+
+get('/index') do 
+
+    id = session[:id].to_i
+    db = SQLite3::Database.new('db/todo2021.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM rating WHERE user_id = ?",id)
+    p "Alla rating från result #{result}"
+    slim(:index, locals:{rating:result})
+end 
+
+post('/users/new') do 
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/db.db')
+      db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',[username,password_digest])
+      redirect('/index')
+    else
+      "Lösenorden matchade inte"
+    end
+  end
