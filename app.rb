c6a3899d18de3a65274dea181cb7632aec9af461
Arvin-Ullvan-NTI-2C <151524993+Arvin-Ullvan-NTI-2C@users.noreply.github.com>
2025-03-18 16:20:28 +0100

    försök till delete

--- app.rb
+++ app.rb
@@ -36,7 +36,6 @@ post('/login') do
 end
 
 get('/priv_profile') do 
-
     id = session[:id].to_i
     db = SQLite3::Database.new('db/db.db')
     db.results_as_hash = true 
@@ -79,16 +78,41 @@ post('/users/new') do
     end
   end
 
-post('/rating/new') do
-  user_id = session[:id]
-  rating = params[:rating].to_i
-  title = params[:title]
-  author = params[:author]
-  series = params[:series]
-  comment = params[:comment]
-  db = SQLite3::Database.new('db/db.db')
-  db.execute('INSERT INTO rating (user_id,rating,comment) VALUES (?,?,?)', [session[:id], rating,comment])
-  db.execute('INSERT INTO media (author, title, series) VALUES (?,?,?)', [author,title,series])
-  redirect('/priv_profile')
-end
+  post('/rating/new') do
+    user_id = session[:id]
+    rating = params[:rating].to_i
+    title = params[:title]
+    author = params[:author]
+    series = params[:series]
+    comment = params[:comment]
+  
+    db = SQLite3::Database.new('db/db.db')
+    db.results_as_hash = true 
+  
+    # Check if the media already exists
+    media = db.execute("SELECT id FROM media WHERE title = ? AND author = ? AND series = ?", [title, author, series]).first
+  
+    if media
+      media_id = media["id"]
+    else
+      # Insert media only if it doesn't exist
+      db.execute('INSERT INTO media (author, title, series) VALUES (?,?,?)', [author, title, series])
+      media_id = db.last_insert_row_id
+    end
+  
+    # Insert rating using the correct media_id
+    db.execute('INSERT INTO rating (user_id, media_id, rating, comment) VALUES (?,?,?,?)', [user_id, media_id, rating, comment])
+  
+    redirect('/priv_profile')
+  end
+  
 
+post('/rating/:rating_id/delete') do
+    rating_id = params[:rating_id].to_i  
+    db = SQLite3::Database.new('db/db.db')
+    db.results_as_hash = true 
+    db.execute("DELETE FROM rating WHERE rating_id = ?", [rating_id])
+    redirect('/priv_profile')
+    
+end
+  
